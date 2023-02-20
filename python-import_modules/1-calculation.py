6c3c3284ac47a57ba38cb96ab703760a1e3c9c18
Islam-19984 <i.hemade@alustudent.com>
2023-02-20 09:02:56 -0800

    1-calculation.py

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-from calculator_1 import add, sub, mul, div
+import calculator_1
 
 a = 10
 b = 5
-if _name_ == '_main_':
-    print("{} + {} = {}".format(a, b, add(a, b)))
-    print("{} - {} = {}".format(a, b, sub(a, b)))
-    print("{} * {} = {}".format(a, b, mul(a, b)))
-    print("{} / {} = {}".format(a, b, div(a, b))
+
+print("Addition of a and b: ", calculator_1.add(a, b))
+print("Subtraction of a and b: ", calculator_1.sub(a, b))
+print("Multiplication of a and b: ", calculator_1.mul(a, b))
+print("Division of a and b: ", calculator_1.div(a, b))
+
