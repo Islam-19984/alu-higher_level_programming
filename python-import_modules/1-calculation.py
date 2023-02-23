e69dd77caa4dd56dbfb9708770e6cb0fef6f0395
Islam-3 <120138935+Islam-19984@users.noreply.github.com>
2023-02-23 11:30:35 -0800

    Update 1-calculation.py

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
-if __name__ == __main__":
-    """print the sum, difference, multiple and quotient of 10 and 5."""
+if _name_ == "_main_":
+    """Print the sum, difference, multiple and quotient of 10 and 5."""
     from calculator_1 import add, sub, mul, div
 
     a = 10
