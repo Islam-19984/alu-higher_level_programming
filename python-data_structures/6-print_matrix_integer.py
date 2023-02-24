b3d80ada9cb61c56e7c202fe878c2a7ad844e15d
Islam-19984 <i.hemade@alustudent.com>
2023-02-24 04:23:35 -0800

    6-print_matrix_integer.py

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -3,8 +3,8 @@ def print_matrix_integer(matrix=[[]]):
     for list_in matrix:
         if not list_:
             print()
-        for item in list_:
-            if item == list_[-1]:
-                print('{:d}'.format(item))
-            else:
-                print('{:d}'.format(item), end=" ")
+    for item in list_:
+        if item == list_[-1]:
+            print('{:d}'.format(item))
+        else:
+            print('{:d}'.format(item), end=" ")
