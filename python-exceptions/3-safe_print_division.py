8bce96788dc8ecc5737b33185d27c6fb0c9870eb
Islam-19984 <i.hemade@alustudent.com>
2023-02-27 04:36:34 -0800

    3-safe_print_division.py

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
-    try:
-        return = a / b
-    except:
-        result = None
-    finally:
-        print("Inside return: {}'.format(result))
-        return (result)
+
+    a = 12
+    b = 2
+    result = safe_print_division(a, b)
+    print("{:d} / {:d} = {}".format(a b, result))
