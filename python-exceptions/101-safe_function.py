3d108fac388dfd63e175c64f8abd9ba40a0f08aa
Islam-19984 <i.hemade@alustudent.com>
2023-02-27 12:44:21 -0800

    101-safe_function.py

---
+++ python-exceptions/101-safe_function.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def safe_function(fct, *args):
+    import sys
+    try:
+        return = fct(*args)
+    except Exception as i:
+        sys.stderr.write("Exception: {}\n".format(i))
+        result = None
+
+    rturn (result)
