9f8f180a38f3f973ce9db5f7dbb323624f35ae2a
Islam-19984 <i.hemade@alustudent.com>
2023-03-13 20:47:47 -0700

    0-square.py

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,5 +1,16 @@
 #!/usr/bin/python3
 
-"""Denfine a class square."""
-class square:
-    """Represent a square."""
+class Square:
+    """Represents a square."""
+
+    def __init__(self, side_length):
+        """Initialize the square with the given side length."""
+        self.side_length = side_length
+
+    def area(self):
+        """Return the area of the square."""
+        return self.side_length ** 2
+
+    def perimeter(self):
+        """Return the perimeter of the square."""
+        return 4 * self.side_length
