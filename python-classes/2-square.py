d790e282d36a9dcceb3f5d006980412d49b3f71f
Islam-19984 <i.hemade@alustudent.com>
2023-03-15 07:17:55 -0700

    2-square.py

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -3,14 +3,14 @@
 
 
 class Square:
-    """Creates a square.
+    """Create a square.
     Private instance attributes: size
     """
 
     def __init__(self, size=0):
-        """Initializes data."""
+        """Initializes date."""
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
-        self.__size = size
+        self.__size = size
