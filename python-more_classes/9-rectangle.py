9e6af287a018d8bcaa999fd748db55593ccf56e3
Islam-19984 <i.hemade@alustudent.com>
2023-03-17 06:48:40 -0700

    9-rectangle.py

--- python-more_classes/9-rectangle.py
+++ python-more_classes/9-rectangle.py
@@ -109,4 +109,4 @@ class Rectangle:
         """Returns the rectangle perimeter"""
         if self.__width == 0 or self.__height == 0:
             return 0
-        return 2 * (self.__width + self.__height)     
+        return 2 * (self.__width + self.__heigh)
