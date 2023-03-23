2472be469e8fe0f7e37361dc62394d76a22bb2bd
Islam-19984 <i.hemade@alustudent.com>
2023-03-23 14:46:40 -0700

    1-my_list.py

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""A class that inherits."""
+
+
+class MyList(list):
+    """Contains list."""
+
+    def print_sorted(self):
+        """Prints sorted list."""
+
+        sorted_list = sorted(self)
+        print(sorted_list)
