5527c890da9f8ebaf0494e25a1218c004441ff8a
Islam-19984 <i.hemade@alustudent.com>
2023-03-24 09:44:33 -0700

    100-magic_string.py

--- python-everything_is_object/100-magic_string.py
+++ python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-magic_string = __import__('100-magic_string').magic_string
-for i in range(10):
-    print(magic_string())
+def magic_string():
+    magic_string.counter = getattr(magic_string, 'counter', 0) + 1
+    return ', '.join(['Holberton']*magic_string.counter)
