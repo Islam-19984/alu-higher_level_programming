7d2a3ddedd1f8c26b77105028aacdc88adbdec5b
Islam-19984 <i.hemada@alustudent.com>
2023-03-31 05:43:24 -0700

    10-genre_id_by_show.sql

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,5 +1,5 @@
 -- list all shows in hbtn_0d_tvsows that have at leaste 1 genre linked
-SELECT tv_shows.title, tv_genres.genre_id
+SELECT tv_shows.title, tv_show_genres.genre_id
        FROM tv_shows
        INNER JOIN tv_shows_genres
        ON tv_show.id = tv_show_genres.show_id
