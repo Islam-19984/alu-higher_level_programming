a82d9580e70d6134c14fb4aaa13c7b88eaa9cc9c
Islam-19984 <i.hemada@alustudent.com>
2023-03-31 01:43:35 -0700

    1-create_user.sql

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
--- Creates a user and password
+-- creates a user and password
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
 ALTER USER 'user_0d_1'@'localhost' IDENTIFIED BY user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@localhost;
