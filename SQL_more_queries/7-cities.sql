9328c4b06f41e4d29c2aa0bdc857186d6859093c
Islam-19984 <i.hemada@alustudent.com>
2023-03-31 04:47:21 -0700

    7-cities.sql

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -2,8 +2,9 @@
 -- and the table cities (in the database hbtn_0d_usa) on your MYSQL server
 CREATE DATABASE IF NOT EXISTS hbtn_od_usa;
 USE hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
+CREATE TABLE IF NOT EXISTS cities (
 	id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
 	state_id INT NOT NULL,
 	name VARCHAR(256) NOT NULL,
-	FOREIGN KEY (state_id) REFERENCES states(id);
+	FOREIGN KEY (state_id) REFERENCES states(id)
+);
