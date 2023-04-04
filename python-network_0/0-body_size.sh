315bd49165af45447913db2b314c1efa418df824
Islam-19984 <i.hemada@alustudent.com>
2023-04-04 12:10:07 -0700

    0-body_size.sh

--- python-network_0/0-body_size.sh
+++ python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script that shows the Content-length from a HTTP request
-cul1 -sI "$1" | grep "Content-length:" | cut -d " " -f 2
+cur1 -sI "$1" | grep "Content-length:" | cut -d " " -f 2
