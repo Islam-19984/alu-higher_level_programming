ce61eeecf75a79e7ad01e8534e966dfa3849b50f
Islam-19984 <i.hemada@alustudent.com>
2023-04-05 06:58:31 -0700

    10-my_github.py

--- python-network_1/10-my_github.py
+++ python-network_1/10-my_github.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
     username = sys.argv[1]
     password = sys.argv[2]
     response = requests.get(
-        url="https://api.github.com/usr",
+        url="https://api.github.com/user",
         auth=(requests.auth.HTTPBasicAuth(
             username,
             password
