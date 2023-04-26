4c7ffc98417540e2af2f80f0d6a9216b08954e5e
Islam-19984 <i.hemada@alustudent.com>
2023-04-26 09:27:27 -0700

    tests

--- python-almost_a_circle/tests/tests_models/test_base.py
+++ python-almost_a_circle/tests/tests_models/test_base.py
@@ -30,7 +30,7 @@ class TestBase(TestCase):
 
     def test_save_to_file(self):
         """Test that the file saves list objects to  file"""
-        Base.Base_nb_objects = 0
+        Base._Base__nb_objects = 0
         Square.save_to_file(None)
 
         self.assertTrue(os.path.isfile("Square.json"))
@@ -46,7 +46,7 @@ class TestBase(TestCase):
         with open("Square.json") as file:
             self.assertEqual(file.read(),
                              '[{"id": 1, "size": 1, "x": 0, "y": 0}]')
-        Base.Base_nb_objects = 0
+        Base._Base__nb_objects = 0
 
         Rectangle.save_to_file(None)
         self.assertTrue(os.path.isfile("Rectangle.json"))
@@ -70,4 +70,4 @@ class TestBase(TestCase):
         self.assertEqual(Base.from_json_string(None), [])
         self.assertEqual(Base.from_json_string('[{"id": 89}]'), [{'id': 89}])
         self.assertEqual(type(Base.from_json_string('[{"id": 89}]')), list)
-        self.assertEqual(Base.from_json_string("[]"), [])
+        self.assertEqual(Base.from_json_string("[]"), [])
