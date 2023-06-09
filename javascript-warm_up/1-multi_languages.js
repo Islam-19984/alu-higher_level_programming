9c8f204457b9181ce9a68c2177c04f2646042e77
Islam-19984 <i.hemada@alustudent.com>
2023-06-09 11:39:34 +0200

    1-multi_languages.js

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -2,4 +2,6 @@
 
 const lines = ['JavaScript is amazing', 'python is cool', 'C is fun'];
 
-for (const line of lines.reverse()) console.log(lines);
+for (const line of lines.reverse()) {
+  console.log(line);
+}
