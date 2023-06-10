d7b63d46f9812a3da651bfe21bceed9a3f02e760
Islam-19984 <i.hemada@alustudent.com>
2023-06-10 20:38:29 +0200

    7-occurrences.js

---
+++ javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+export.nOccurences = function (list, searchElement) {
+  let nOccurences = 0;
+  for (let i = 0; i < list.length; i++) {
+    if (searchElement === list[i]) {
+      nOccurences++;
+    }
+  }
+  return nOccurences;
+};
