94139faf7bd74c99c3ee7c2f034c4d623f3d9490
Islam-19984 <i.hemada@alustudent.com>
2023-06-10 21:39:19 +0200

    101-sorted.js

--- javascript_objects_scopes_closures/101-sorted.js
+++ javascript_objects_scopes_closures/101-sorted.js
@@ -2,7 +2,7 @@
 const dict = require('./101-data.js').dict;
 let newDict = {};
 for (let key in dict) {
-  if (newDict[dict[key]] === underfined) {
+  if (newDict[dict[key]] === undefined) {
     newDict[dict[key]] = [key];
   } else {
     newDict[dict[key]].push(key);
