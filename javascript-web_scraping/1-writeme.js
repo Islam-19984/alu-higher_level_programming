46249ce4058c847921125d82fed43fe0a4b0fd70
Islam-19984 <i.hemada@alustudent.com>
2023-06-11 14:20:45 +0200

    1-writeme.js

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
 const fs = require('fs');
-const filePath = process.argv[3];
+const filePath = process.argv[2];
+const stringToWrite = process.argv[3]
 
 fs.writeFileSync(filePath, stringToWrite, 'utf-8', function (error) {
   if (error) {
