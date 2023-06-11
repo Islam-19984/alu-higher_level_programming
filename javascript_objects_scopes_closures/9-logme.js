f8501c0afe25670a4274fd3d680fe1e8dc12a3e8
Islam-19984 <i.hemada@alustudent.com>
2023-06-11 13:39:05 +0200

    9-logme.js

--- javascript_objects_scopes_closures/9-logme.js
+++ javascript_objects_scopes_closures/9-logme.js
@@ -1,5 +1,9 @@
 #!/usr/bin/node
 
-exports.converter = function (base) {
-  return (number = 0) => number.toString(base);
-};
+function logMe (item) {
+  if (!this.count) this.count = 0;
+  console.log('${this.count}: ${item}');
+  this.count++;
+}
+
+module.exports = { logMe };
