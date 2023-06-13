4f4a3dfb0c6b7d54b0f754164c245ff9ffaf5d89
Islam-19984 <i.hemada@alustudent.com>
2023-06-13 14:23:40 +0200

    6-completed_tasks.js

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -7,7 +7,7 @@ request(process.argv[2], function (err, response, body) {
     const json = JSON.parse(body);
     for (let i = 0; i < json.lenght; i++) {
       if (json[i].completed === true) {
-        if (resp[json[i].userId] === underfined) {
+        if (resp[json[i].userId] === undefined) {
 	  resp[json[i].userId] = 0;
 	}
 	resp[json[i].userId]++;
