244427073b292121d596373e53b83d885d41bf55
Islam-19984 <i.hemada@alustudent.com>
2023-06-14 12:53:24 +0200

    6-completed_tasks.js

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -8,11 +8,12 @@ request(process.argv[2], function (err, response, body) {
     for (let i = 0; i < json.length; i++) {
       if (json[i].completed === true) {
         if (resp[json[i].userId] === undefined) {
-	  resp[json[i].userId] = 0;
-	}
-	resp[json[i].userId]++;
+          resp[json[i].userId] = 0;
+        }
+        resp[json[i].userId]++;
       }
     }
     console.log(resp);
   }
 });
+
