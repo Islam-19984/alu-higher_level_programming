c1a52896ea54c8652f266fff91851e683c60997d
Islam-19984 <i.hemada@alustudent.com>
2023-06-15 22:47:17 +0200

    101-starwars_characters.js

--- javascript-web_scraping/101-starwars_characters.js
+++ javascript-web_scraping/101-starwars_characters.js
@@ -1,54 +1,28 @@
 #!/usr/bin/node
 const request = require('request');
 
-function printCharacters(movieId) {
-  const baseUrl = 'https://swapi.dev/api/films/';
-  const movieUrl = `${baseUrl}${movieId}/`;
+const movieId = process.argv[2];
 
-  request.get(movieUrl, (error, response, body) => {
-    if (error) {
-      console.error('An error occurred:', error);
-      return;
-    }
+request(`https://swapi.dev/api/films/${movieId}/`, (error, response, body) => {
+  if (error) {
+    console.error('Error:', error);
+  } else if (response.statusCode !== 200) {
+    console.error('Status:', response.statusCode);
+  } else {
+    const film = JSON.parse(body);
+    const characters = film.characters;
 
-    if (response.statusCode !== 200) {
-      console.error('Invalid request:', response.statusCode);
-      return;
-    }
-
-    const movieData = JSON.parse(body);
-    console.log(`Characters in ${movieData.title}:`);
-
-    const characterPromises = movieData.characters.map((characterUrl) => {
-      return new Promise((resolve, reject) => {
-        request.get(characterUrl, (error, response, body) => {
-          if (error) {
-            reject(error);
-            return;
-          }
-
-          if (response.statusCode !== 200) {
-            reject(`Invalid request: ${response.statusCode}`);
-            return;
-          }
-
-          const characterData = JSON.parse(body);
-          resolve(characterData.name);
-        });
+    characters.forEach((characterUrl) => {
+      request(characterUrl, (error, response, body) => {
+        if (error) {
+          console.error('Error:', error);
+        } else if (response.statusCode !== 200) {
+          console.error('Status:', response.statusCode);
+        } else {
+          const character = JSON.parse(body);
+          console.log(character.name);
+        }
       });
     });
-
-    Promise.all(characterPromises)
-      .then((characterNames) => {
-        characterNames.forEach((characterName) => {
-          console.log(characterName);
-        });
-      })
-      .catch((error) => {
-        console.error('An error occurred:', error);
-      });
-  });
-}
-
-const movieId = process.argv[2];
-printCharacters(movieId);
+  }
+});
