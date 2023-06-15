935afe39a61c04013d37cb46da7c7798abb04d67
Islam-19984 <i.hemada@alustudent.com>
2023-06-15 23:20:44 +0200

    101-starwars_characters.js

--- javascript-web_scraping/101-starwars_characters.js
+++ javascript-web_scraping/101-starwars_characters.js
@@ -1,28 +1,40 @@
 #!/usr/bin/node
 const request = require('request');
 
-const movieId = process.argv[2];
+function fetchCharacters(movieId) {
+  const url = `https://swapi.dev/api/films/${movieId}/`;
 
-request(`https://swapi.dev/api/films/${movieId}/`, (error, response, body) => {
-  if (error) {
-    console.error('Error:', error);
-  } else if (response.statusCode !== 200) {
-    console.error('Status:', response.statusCode);
-  } else {
+  request.get(url, (error, response, body) => {
+    if (error) {
+      console.error('Error:', error);
+      return;
+    }
+
+    if (response.statusCode !== 200) {
+      console.error('Invalid response:', response.statusCode);
+      return;
+    }
+
     const film = JSON.parse(body);
     const characters = film.characters;
 
-    characters.forEach((characterUrl) => {
-      request(characterUrl, (error, response, body) => {
+    characters.forEach(characterUrl => {
+      request.get(characterUrl, (error, response, body) => {
         if (error) {
           console.error('Error:', error);
-        } else if (response.statusCode !== 200) {
-          console.error('Status:', response.statusCode);
-        } else {
-          const character = JSON.parse(body);
-          console.log(character.name);
+          return;
         }
+
+        if (response.statusCode !== 200) {
+          console.error('Invalid response:', response.statusCode);
+          return;
+        }
+
+        const character = JSON.parse(body);
+        console.log(character.name);
       });
     });
-  }
-});
+  });
+}
+
+fetchCharacters(3);
